9e1d2bb432320989be3a601d4cb03058d608e4c8
Steve Hu <stevehu@gmail.com>
2015-04-29 22:10:29 -0400

    update catalog page

--- view/catalog/app.js
+++ view/catalog/app.js
@@ -5,6 +5,6 @@ var CatalogPage = require('./scripts/component/CatalogPage');
 require('./assets/css/demo.css');
 
 React.render(
-  <CatalogPage />,
-  document.getElementById('content')
+  <CatalogPage url={'http://example:8080/api/rs'} perPage={10} />,
+  document.getElementById('catalog_content')
 );
--- view/catalog/scripts/component/CatalogPage.js
+++ view/catalog/scripts/component/CatalogPage.js
@@ -6,9 +6,49 @@ var CheckoutButton = require('./CheckoutButton');
 var TreePath = require('./TreePath');
 var SearchForm = require('./SearchForm');
 var WebApi = require('../WebApi');
+var ReactPaginate = require('react-paginate');
+var $ = require('jquery');
+
 WebApi.getAllProducts();
 
 var CatalogPage = React.createClass({displayName: "CatalogPage",
+
+    loadCatalogFromServer: function() {
+        $.ajax({
+            url      : this.props.url,
+            data     : {cmd: "{readOnly: true, category:'catalog', name:'getCatalogProduct', data: {host:'example', pageSize:" + this.props.perPage +", pageNo: " + this.state.offset + "}}"},
+            //data: {cmd: this.props.perPage},
+            dataType : 'json',
+            type     : 'GET',
+
+            success: function(data) {
+                this.setState({data: data.comments, pageNum: Math.ceil(data.meta.total_count / data.meta.limit)});
+            }.bind(this),
+
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    handlePageClick: function(data) {
+        var selected = data.selected;
+        var offset = Math.ceil(selected * this.props.perPage);
+
+        this.setState({offset: offset}, function() {
+            this.loadCatalogFromServer();
+        }.bind(this));
+
+        this.loadCatalogFromServer();
+    },
+
+    getInitialState: function() {
+        return {data: [], offset: 0};
+    },
+
+    componentDidMount: function() {
+        this.loadCatalogFromServer();
+    },
 
     render: function() {
         return (
@@ -27,6 +67,17 @@ var CatalogPage = React.createClass({displayName: "CatalogPage",
                     </div>
                 </div>
                 <ProductList/>
+                <ReactPaginate previousLabel={"previous"}
+                               nextLabel={"next"}
+                               breakLabel={<li className="break"><a href="">...</a></li>}
+                               pageNum={this.state.pageNum}
+                               marginPagesDisplayed={1}
+                               pageRangeDisplayed={2}
+                               clickCallback={this.handlePageClick}
+                               containerClassName={"pagination"}
+                               subContainerClassName={"pages pagination"}
+                               activeClass={"active"} />
+
             </div>
         );
     }
